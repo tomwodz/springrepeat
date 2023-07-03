8dd34bc03f46c2d312ce6ede4f3e02396eb40c37
twodzinski@op.pl <twodzinski@op.pl>
2023-07-03 22:33:24 +0200

    SnakeYAML

--- src/main/java/pl/jjr/tomwodz/springrepeat/config/AppConfig.java
+++ src/main/java/pl/jjr/tomwodz/springrepeat/config/AppConfig.java
@@ -1,17 +1,22 @@
 package pl.jjr.tomwodz.springrepeat.config;
 
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 
+import java.util.Objects;
+
 @Configuration
 public class AppConfig {
     @Bean
-    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
-        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
-        configurer.setLocation(new ClassPathResource("application.properties"));
-        return configurer;
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+        PropertySourcesPlaceholderConfigurer propertyConfigurer = new PropertySourcesPlaceholderConfigurer();
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        yaml.setResources(new ClassPathResource("application.yml"));
+        propertyConfigurer.setProperties(Objects.requireNonNull(yaml.getObject()));
+        return propertyConfigurer;
     }
 
 }
